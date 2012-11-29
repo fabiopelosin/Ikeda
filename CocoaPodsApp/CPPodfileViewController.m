2ad1c1f0648689603608d906c2b93f41dbe0557e
Keith Smiley <keithbodesmiley@gmail.com>
2012-11-28 22:46:42 -0500

    Applied open pod spec fix to other occurrence. Should call from the same place in the future.

--- CocoaPodsApp/CPPodfileViewController.m
+++ CocoaPodsApp/CPPodfileViewController.m
@@ -53,7 +53,17 @@
 }
 
 - (IBAction)openPodfileAction:(NSButton*)sender {
-  [[NSWorkspace sharedWorkspace] openFile:[_podfile path]];
+    if (![[NSWorkspace sharedWorkspace] openFile:[_podfile path]])
+    {
+        NSString *defaultApplication = (__bridge NSString *)(LSCopyDefaultRoleHandlerForContentType(kUTTypePlainText, kLSRolesEditor));
+        
+        if (![[NSWorkspace sharedWorkspace] openFile:[_podfile path]
+                                     withApplication:[[NSWorkspace sharedWorkspace]
+                                                      absolutePathForAppBundleWithIdentifier:defaultApplication]])
+        {
+            NSLog(@"Failed to open podspec at: %@", [_podfile path]);
+        }
+    }
 }
 
 - (void) runExecutableWithTitle:(NSString*)title arguments:(NSArray*)arguments {
